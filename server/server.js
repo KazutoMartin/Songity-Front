f1d9d21002d06bb33d6f00e0d0c8a6cc4734ded0
KazutoMartin <75247334+KazutoMartin@users.noreply.github.com>
2022-02-11 20:32:45 +0330

    First Commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const express = require("express");
+const publicPath = path.join(__dirname, "..", "public");
+app.use(express.static(publicPath));
+const path = require("path");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+const publicPath = path.join(__dirname, "..", "public");
+app.use(express.static(publicPath));
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
